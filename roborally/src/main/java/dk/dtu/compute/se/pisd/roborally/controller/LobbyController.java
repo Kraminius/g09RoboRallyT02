f43b8e5c6a4d3e31ecdb012765127dff5f5f12bd
mikkelJurs <s224279@student.dtu.dk>
2023-06-03 19:44:10 +0200

    Added LobbyController base code. Not being used right now. Fixing later.

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/LobbyController.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/LobbyController.java
@@ -1,15 +1,21 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import dk.dtu.compute.se.pisd.roborally.model.GameLobby;
 import dk.dtu.compute.se.pisd.roborally.model.GameSettings;
-import dk.dtu.compute.se.pisd.roborally.view.Lobby;
+import dk.dtu.compute.se.pisd.roborally.model.LobbyManager;
+import dk.dtu.compute.se.pisd.roborally.view.LobbyWindow;
 
 public class LobbyController {
-    private Lobby lobby;
-    private GameSettings gameSettings;
+    private LobbyManager lobbyManager;
 
     public LobbyController() {
-        gameSettings = new GameSettings();
-        lobby = new Lobby(gameSettings);
+        this.lobbyManager = LobbyManager.getInstance();
     }
 
+    public void openCreateGameScene() {
+        GameSettings gameSettings = new GameSettings(); // create a new game settings
+        String lobbyID = String.valueOf(lobbyManager.getLobbies().size() + 1); // generate lobby ID based on the number of existing lobbies
+        GameLobby gameLobby = new GameLobby(lobbyID, gameSettings); // create a new game lobby
+        lobbyManager.createGame(gameLobby); // Pass the GameLobby instance to the createGame method
+    }
 }
